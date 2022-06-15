619b3fc8d3b87c4d8bc1078a41426eb189658ffe
rrebuffo <mauro@mehir.com.ar>
2022-06-15 15:25:29 -0300

    Update to 1.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
