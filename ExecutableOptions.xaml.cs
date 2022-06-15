1b9feaa00ea2551e28bb4bfa6cded2f8d31fe572
rrebuffo <mauro@mehir.com.ar>
2022-06-15 15:13:13 -0300

    Enable copy and paste for command list

--- ExecutableOptions.xaml.cs
+++ ExecutableOptions.xaml.cs
@@ -47,7 +47,7 @@ namespace CasparLauncher
 
         private void AddCommand(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
         private void RemoveCommand(object sender, RoutedEventArgs e)
@@ -71,6 +71,52 @@ namespace CasparLauncher
             {
                 return;
             }
+        }
+
+        private void Copy(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (sender is DataGrid commands_dg)
+            {
+                StringBuilder commandList = new StringBuilder();
+                var first = false;
+                foreach (Command c in commands_dg.SelectedItems)
+                {
+                    if (!first) first = true;
+                    else commandList.Append('\n');
+                    commandList.Append(c.Value);
+                }
+                Clipboard.SetData(DataFormats.UnicodeText, commandList.ToString());
+            }
+        }
+
+        private void CanCopy(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CanPaste(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+            e.Handled = true;
+        }
+
+        private void Paste(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                List<string> values = new List<string>();
+                var data = "";
+                if (Clipboard.ContainsData(DataFormats.CommaSeparatedValue) || Clipboard.ContainsData(DataFormats.UnicodeText))
+                {
+                    if (Clipboard.ContainsData(DataFormats.CommaSeparatedValue)) data = (string)Clipboard.GetData(DataFormats.CommaSeparatedValue);
+                    else if (Clipboard.ContainsData(DataFormats.UnicodeText)) data = (string)Clipboard.GetData(DataFormats.UnicodeText);
+                    else return;
+                    data = data.Replace("\r\n", "\n");
+                    values.AddRange(data.Split('\n'));
+                }
+                foreach (string value in values) if (value != "") Executable.Commands.Add(new Command() { Value = value });
+            }
+            catch { }
         }
     }
 }
